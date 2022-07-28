40bdae3db313e4dc4a07a312fa1c8e4f4b90bb09
Jakob Schlichting <jakob.schlichting@mail.de>
2022-07-28 18:16:13 +0200

    switching devices

---
+++ nvim/lua/jakobschlichting/init.lua
@@ -0,0 +1,36 @@
+local augroup = vim.api.nvim_create_augroup
+JakobSchlichtingGroup = augroup('JakobSchlichting', {})
+
+require('jakobschlichting.packer')
+require('jakobschlichting.set')
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufEnter", "BufWinEnter", "TabEnter"}, {
+    group = JakobSchlichtingGroup,
+    pattern = "*.rs",
+    callback = function()
+        require("lsp_extensions").inlay_hints{}
+    end
+})
+
+autocmd({"BufWritePre"}, {
+    group = JakobSchlichtingGroup,
+    pattern = "*",
+    command = "%s/\\s\\+$//e",
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
