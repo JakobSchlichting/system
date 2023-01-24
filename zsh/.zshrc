13c2a2bce21cb54ec0cee454706c9f7bb1afe79c
Jakob Schlichting <jakob.schlichting@mail.de>
2023-01-24 17:19:38 +0100

    feat: added zsh config

---
+++ zsh/.zshrc
@@ -0,0 +1,8 @@
+source ~/.zsh/alias.zsh
+source ~/.zsh/history.zsh
+source ~/.zsh/opts.zsh
+source ~/.zsh/env.zsh
+source ~/.zsh/completion.zsh
+
+eval "$(starship init zsh)"
+neofetch
---
+++ zsh/zsh/alias.zsh
@@ -0,0 +1,18 @@
+# vi alias
+alias vi='nvim'
+alias vim='nvim'
+
+# clear terminal
+alias cls='clear'
+
+# python
+alias python='python3'
+
+# working with directory
+alias ll='ls -la'
+alias la='ls -A'
+alias l='ls -CF'
+alias dir='ls -la'
+alias ....='cd ../..'
+alias ......='cd ../../..'
+alias ........='cd ../../../..'
