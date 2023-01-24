772350d775cafcb608abb605acedfc3013420788
Jakob Schlichting <jakob.schlichting@mail.de>
2023-01-24 17:25:10 +0100

    quickfix

--- zsh/zsh/env.zsh
+++ zsh/zsh/env.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 export PATH=$PATH:~/bin:~/Qt/6.2.4/macos/bin:~/tools/system/scripts:/usr/local/opt/libpq/bin
-export GITHUB_PAT="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"
+export GITHUB_PAT=""
