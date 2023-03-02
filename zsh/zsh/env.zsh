eab6b9862ffc82a3f117c1635234c3c01dc056be
Jakob Schlichting <jakob.schlichting@mail.de>
2023-03-02 22:13:32 +0100

    feat: added node and haskell toolchain

--- zsh/zsh/env.zsh
+++ zsh/zsh/env.zsh
@@ -1,4 +1,13 @@
 #!/bin/zsh
 
-export PATH=$PATH:~/bin:~/Qt/6.2.4/macos/bin:~/tools/system/scripts:/usr/local/opt/libpq/bin
+# Load rust toolchain and cargo applicaations
+source ~/.cargo/env
+
+# Load Haskell toolchain
+[ -f "/home/jakobschlichting/.ghcup/env" ] && source "/home/jakobschlichting/.ghcup/env" # ghcup-env
+
+# NodeJS toolchain
+eval "$(fnm env --use-on-cd)"
+
+#export PATH=$PATH:~/bin:~/Qt/6.2.4/macos/bin:~/tools/system/scripts:/usr/local/opt/libpq/bin
 export GITHUB_PAT=""
