63c98a05ad6f7e7d993d293afe453c423accf101
Jakob Schlichting <jakob.schlichting@mail.de>
2023-06-20 09:43:30 +0200

    feat: alacritty

--- zsh/.config/zsh/completion.zsh
+++ zsh/.config/zsh/completion.zsh
@@ -1,11 +1,11 @@
 # enable completion features
 autoload -Uz compinit
-compinit -d ~/.cache/zcompdump
+compinit -d $HOME/.cache/zcompdump
 zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' # case insensitive tab completion
 
 # OpenShift autocomplete, when oc exists
-if [ $commands[oc] ]; then                                                    
-    source <(oc completion zsh)                                              
-    compdef _oc oc            
+if [ $commands[oc] ]; then
+    source <(oc completion zsh)
+    compdef _oc oc
 fi
