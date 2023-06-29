9f5adbf7c296c3bf3cc4d7369c8ad132a95e3d9d
Jakob Schlichting <jakob.schlichting@mail.de>
2023-06-29 09:18:40 +0200

    feat: added swift suite to nvim

---
+++ neovim/.config/nvim/after/plugin/copilot.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("i", "<C-j>", '<Cmd>call copilot#accept("\\<CR>")')
+vim.g['copilot_no_tab_map'] = true
--- neovim/.config/nvim/lua/jakobschlichting/packer.lua
+++ neovim/.config/nvim/lua/jakobschlichting/packer.lua
@@ -24,6 +24,21 @@ return require("packer").startup(function()
 		}
 	}
 
+    -- Swift suite
+    use {
+        'xbase-lab/xbase',
+        run = 'make install', -- or "make install && make free_space" (not recommended, longer build time)
+        requires = {
+            "neovim/nvim-lspconfig",
+            -- "nvim-telescope/telescope.nvim", -- optional
+            -- "nvim-lua/plenary.nvim", -- optional/requirement of telescope.nvim
+            -- "stevearc/dressing.nvim", -- optional (in case you don't use telescope but something else)
+        },
+        config = function()
+            require'xbase'.setup({})  -- see default configuration bellow
+        end
+    }
+
     -- Fuzzy finder
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
