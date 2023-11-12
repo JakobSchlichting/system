2eaf334a2047f43d00a2fc204a9949ee468be09e
Jakob Schlichting <jakob.schlichting@mail.de>
2023-11-12 22:40:48 +0100

    feat: removed old nvim configuration

--- neovim/.config/nvim/after/plugin/copilot.lua
+++
@@ -1,2 +0,0 @@
--- vim.keymap.set("i", "<C-j>", '<Cmd>call copilot#accept("\\<CR>")')
--- vim.g['copilot_no_tab_map'] = true
--- neovim/.config/nvim/lua/jakobschlichting/keymap.lua
+++
@@ -1,3 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux new-window tmuxifier<CR>")
