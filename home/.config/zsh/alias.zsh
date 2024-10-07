bee2e404a9830b07b724ddd5b6e2ca6d13b80d6e
Jakob Schlichting <tech.schlichting@protonmail.com>
2024-10-07 15:06:37 +0200

    feat: added bat alias and made all aliases dependend on the existens of the command

--- home/.config/zsh/alias.zsh
+++ home/.config/zsh/alias.zsh
@@ -1,6 +1,12 @@
 # vi alias
-alias vi='nvim'
-alias vim='nvim'
+if command -v nvim &> /dev/null
+then
+    alias vi='nvim'
+    alias vim='nvim'
+elif command -v vim &> /dev/null
+then
+    alias vi='vim'
+fi
 
 # clear terminal
 alias cls='clear'
@@ -9,6 +15,14 @@ alias cls='clear'
 alias python='python3'
 
 # working with directory
-alias ls='eza'
-alias ll='eza -la'
-alias dir='eza'
+if command -v eza  &> /dev/null
+then
+    alias ls='eza'
+    alias ll='eza -la'
+    alias dir='eza' # for the windows people and still need to drop thew bad habits
+fi
+
+if command -v bat &> /dev/null
+then
+    alias cat='bat'
+fi
