a28fa361eb75837e0cd19bd86b60286ad7836799
Jakob Schlichting <contact@schlichting.dev>
2025-09-18 10:52:25 +0200

    fix: removed duplicate ai config

--- nixos/artificial_intelligence.nix
+++ nixos/artificial_intelligence.nix
@@ -1,11 +1,6 @@
 { pkgs, pkgs-unstable, config, lib, ... }:
 
 {
-    # services.ollama = {
-    #     enable = true;
-    #     loadModels = [ "llama3.2:3b" "llama3.2-vision:11b" "phi4:14b" "deepseek-r1:7b" "dolphin3:8b" "smallthinker:3b" "nomic-embed-text" ];
-    #     acceleration = lib.mkIf (config.hardware.nvidia != null) "cuda";
-    # };
     environment.systemPackages = [
         (pkgs-unstable.ollama.override { 
             acceleration = "cuda";
