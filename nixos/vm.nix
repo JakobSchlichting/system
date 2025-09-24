d80e674ee983336ff8c6ec43eec2f7d1ea506816
Jakob Schlichting <contact@schlichting.dev>
2025-09-24 08:20:39 +0200

    fix: vm isolate network

--- nixos/vm.nix
+++ nixos/vm.nix
@@ -2,10 +2,20 @@
 
 {
     users.groups.libvirtd.members = ["js"];
+    networking = {
+        bridges = {
+            br_vm_iso_1.interfaces = [];
+        };
+    };
+    networking.firewall.extraCommands = ''
+        iptables -A INPUT -i br_vm_iso_1 -j DROP
+        iptables -A FORWARD -i br_vm_iso_1 -j DROP
+    '';
     virtualisation.libvirtd = {
         enable = true;
         qemu = {
             runAsRoot = false;
+            swtpm.enable = true;
         };
     };
     virtualisation.spiceUSBRedirection.enable = true;
